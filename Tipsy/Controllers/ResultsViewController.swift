2d819ed4d8efa1cd20977d93b76dcee151e33444
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2023-09-07 09:14:22 -0300

    up to step 3 done.

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Ednan R. Frizzera Filho on 07/09/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet var totalLabel: UILabel!
+    @IBOutlet var settingsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+}
